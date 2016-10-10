f0ad7fc03251718b6b267d386ff111441c3ce948
ZooMic <marcin.lichota@gmail.com>
2016-10-11 00:32:18 +0200

    standard commit

---
+++ lab_1/moj_program.c
@@ -0,0 +1,37 @@
+#include<stdlib.h>
+#include<stdio.h>
+#include<time.h>
+#include"pomiar_czasu.h"
+
+const int liczba = 100000;
+
+int main(){
+
+  double a, b, c;
+  int i,j,k,l;
+
+  k = -50000;
+  inicjuj_czas();
+  for(i=0;i<liczba;i++){
+
+    printf("%d ",k+i);
+
+  }
+  printf("\n");
+  printf("Czas wykonania %d operacji wejscia/wyjscia: \n",liczba);
+  drukuj_czas();
+
+  a = 1.000001;
+  double t1 = czas_zegara();
+  double t2 = czas_CPU();
+  for(i=0;i<liczba;i++){
+
+    a = 1.000001*a+0.000001;
+
+  }
+  printf("Wynik operacji arytmetycznych: %lf\n", a);
+  printf("Czas wykonania %d operacji arytmetycznych: \n",liczba);
+  t1 = czas_zegara() - t1;
+  t2 = czas_CPU() - t2;
+
+}

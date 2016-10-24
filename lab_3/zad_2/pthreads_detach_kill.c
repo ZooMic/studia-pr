487930eaedebaa712e39fb12c4ab631dbd4ef5db
ZooMic <marcin.lichota@gmail.com>
2016-10-24 21:05:51 +0200

    Poprawka do zadan z lab 3

--- lab_3/zad_2/pthreads_detach_kill.c
+++ lab_3/zad_2/pthreads_detach_kill.c
@@ -1,39 +1,27 @@
 #include<stdlib.h>
 #include<stdio.h>
 #include<pthread.h>
-
-// rzutowanie przekazanego argumentu
 void * wypisywanie_swoich_id(void *arg_wsk) {
     int id;
     id = *((int*) arg_wsk);
-    // wywołanie pthread_self
     pthread_t id_self = pthread_self();
     printf("Przekazany ID %d, pthread_self %u\n", id, id_self);
-    // printf("Pthread_self %u\n", id_self);
-	//pthread_testcancel();
 	return(NULL);
 }
-
 int main()
 {
-	pthread_t tid;
 	pthread_attr_t attr;
 	void *wynik;
 	int i;
     int n = 10;
+    pthread_t *tid = malloc(sizeof(pthread_t) * n);
+    int *id = malloc(sizeof(int) * n);
     for(int a = 0; a < n ; a++) {
-        pthread_create(&tid, NULL, wypisywanie_swoich_id, &a); // DODANE
-
-        // sleep(1); // czas na uruchomienie watku
-        // pthread_cancel(tid);
-        // if (wynik == PTHREAD_CANCELED)
-    	// 	printf("%d KILLED\n", a);
-    	// else
-    	// 	printf("%d NOT KILLED\n", a);
+        id[a] = a;
+        pthread_create(&tid[a], NULL, wypisywanie_swoich_id, &id[a]); // DODANE
     }
-    //pthread_join(tid, &wynik);
     for(int a = 0; a < n; a++) {
-        pthread_join(tid, &wynik); // DODANE
+        pthread_join(tid[a], &wynik); // DODANE
     }
 	pthread_exit(NULL); // co stanie sie gdy uzyjemy exit(0)?
 }
--- lab_3/zad_3/pthreads_detach_kill.c
+++ lab_3/zad_3/pthreads_detach_kill.c
@@ -7,16 +7,17 @@ typedef struct {
     double b;
     int c;
 } struktura;
-
-
 void * operacje_na_strukturze(void *arg_wsk) {
-    struktura s_lokalna;
-    s_lokalna.a = 10;
-    s_lokalna.b = 21;
-    s_lokalna.c = 30;
-    struktura *s_wsk = (struktura *) arg_wsk;
-    printf("Watek: s_lokalna.a = %d, s_lokalna.b = %lf, s_lokalna.c = %d\n", s_lokalna.a, s_lokalna.b, s_lokalna.c);
-    printf("Zewne: s_wsk->a = %d, s_wsk->b = %lf, s_wsk->c = %d\n\n", s_wsk->a, s_wsk->b, s_wsk->c);
+    struktura s_l;
+    struktura *s_g = (struktura *) arg_wsk;
+    s_l.a = s_g->a;
+    s_l.b = s_g->b;
+    s_l.c = s_g->c;
+    s_l.a++;
+    s_l.b++;
+    s_l.c++;
+    printf("Lokalna: a = %d, b = %lf, c = %d\n", s_l.a, s_l.b, s_l.c);
+    printf("Globalna: a = %d, b = %lf, c = %d\n\n", s_g->a, s_g->b, s_g->c);
 	return(NULL);
 }
 
@@ -26,23 +27,18 @@ int main()
     s.a = 1;
     s.b = 2.1;
     s.c = 3;
-
-	pthread_t tid;
-	pthread_attr_t attr;
-	void *wynik;
-	int i;
-    int n = 10;
-    for(int a = 0; a < n ; a++) {
-        pthread_create(&tid, NULL, operacje_na_strukturze, (void *)&s); // DODANE
-        //sleep(1); // czas na uruchomienie watku
-        // pthread_cancel(tid);
-        // if (wynik == PTHREAD_CANCELED)
-    	// 	printf("%d KILLED\n", a);
-    	// else
-    	// 	printf("%d NOT KILLED\n", a);
-    }
-    for(int a = 0; a < n; a++) {
-        pthread_join(tid, &wynik); // DODANE
-    }
-	pthread_exit(NULL); // co stanie sie gdy uzyjemy exit(0)?
+    void *wynik;
+  	int i;
+      int n = 10;
+      pthread_t *tid = malloc(sizeof(pthread_t) * n);
+      int *id = malloc(sizeof(int) * n);
+      for(int a = 0; a < n ; a++) {
+          id[a] = a;
+          pthread_create(&tid[a], NULL, operacje_na_strukturze, (void *)&s); // DODANE
+      }
+      for(int a = 0; a < n; a++) {
+          pthread_join(tid[a], &wynik); // DODANE
+      }
+    printf("Globalna: a = %d, b = %lf, c = %d\n\n", s_g->a, s_g->b, s_g->c);
+  	pthread_exit(NULL); // co stanie sie gdy uzyjemy exit(0)?
 }

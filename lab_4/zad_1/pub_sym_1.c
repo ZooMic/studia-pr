917c56be5e4e327edf000b0a07d00f3c657b98b6
zoomic <marcin.lichota@gmail.com>
2016-10-25 16:15:10 +0200

    Fix

--- lab_4/zad_1/pub_sym_1.c
+++ lab_4/zad_1/pub_sym_1.c
@@ -5,7 +5,7 @@
 
 struct kufle {
   // tab_kufli[] = [0 kufel wolnych] [= 1 kufel zajety]
-  pthread_mutex_t *	tab_mut_kuff;
+  pthread_mutex_t mut_kuff;
   int * tab_kufli;
   int l_kf;
 } kuf;
@@ -14,25 +14,28 @@ pthread_mutex_t mut_kran;
 void * watek_klient (void * arg_wsk){
   int moj_id = * ((int *)arg_wsk);
   int a, i, j, kufel, result;
+  int wybralem = 0;
   a = 0;
   int ile_musze_wypic = 2;
   printf("Klient %d, wchodzÄ do pubu\n", moj_id);
   for(i=0; i<ile_musze_wypic; i++){
     printf("Klient %d, szuka kufla\n", moj_id);
-    a = 0;
+
     while(1) {
-      pthread_mutex_lock(&kuf.tab_mut_kuff[a]); // Tylko kufel 1
-      //pthread_mutex_trylock(&kuf.tab_mut_kuff[a]); // Wszystkie kufle
-      if(kuf.tab_kufli[a] == 0) {
-          kuf.tab_kufli[a] = 1;
-          kufel = a;
-          printf("Klient %d, wybrał kufel %d\n",moj_id,a);
-          break;
-      } else if(a == kuf.l_kf - 1) {
-          a = 0;
-      } else {
-          a++;
-      }
+        pthread_mutex_lock(&kuf.mut_kuff);
+        for(a = 0; a < kuf.l_kf;a++){
+              if(kuf.tab_kufli[a] == 0) {
+                  kuf.tab_kufli[a] = 1;
+                  kufel = a;
+                  printf("Klient %d, wybrał kufel %d\n",moj_id,a);
+                  wybralem = 1;
+                  break;
+              }
+        }
+        pthread_mutex_unlock(&kuf.mut_kuff);
+        if(wybralem) {
+            break;
+        }
     }
     j=0;
     printf("Klient %d, nalewam z kranu %d\n", moj_id, j);
@@ -42,8 +45,9 @@ void * watek_klient (void * arg_wsk){
     printf("Klient %d, pije\n", moj_id);
     nanosleep((struct timespec[]){{0, 500000000L}}, NULL);
     printf("Klient %d, odkladam kufel\n", moj_id);
+    pthread_mutex_lock(&kuf.mut_kuff);
     kuf.tab_kufli[kufel] = 0;
-    pthread_mutex_unlock(&kuf.tab_mut_kuff[kufel]);
+    pthread_mutex_unlock(&kuf.mut_kuff);
   }
   printf("Klient %d,!wychodzi z pubu\n", moj_id);
   return(NULL);
@@ -64,12 +68,11 @@ main(){
     tab_klient_id[i]=i;
   }
   kuf.tab_kufli = (int *) malloc(l_kf * sizeof(int));
-  kuf.tab_mut_kuff = (pthread_mutex_t *) malloc(l_kf * sizeof(pthread_mutex_t));
   kuf.l_kf = l_kf;
+  pthread_mutex_init(&kuf.mut_kuff, NULL);
   for(i=0;i<l_kf;i++)
   {
     kuf.tab_kufli[i] = 0;
-    pthread_mutex_init(&kuf.tab_mut_kuff[i], NULL);
   }
   printf("\nOtwieramy pub (simple)!\n");
   pthread_mutex_init(&mut_kran, NULL);
